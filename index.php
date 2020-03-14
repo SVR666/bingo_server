8cf8ed1ba4b2c375764bb6eed2e91f18c0d7f263
Sreeraj V R <40494878+SVR666@users.noreply.github.com>
2020-03-14 22:01:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-  echo "<h2><u>FINAL YEAR PROJECT :- Crystal Punch (ANDROID GAME)</u></h2>
-        <h3>UNIVERSITY :- KTU </h3>
-        <h3>COLLEGE :- COLLEGE OF ENGINEERING KALLOOPPARA </h3>
-        <h3>COURSE :- BTECH </h3>
-        <h3>BRANCH :- CSE </h3>
-        <h3>YEAR :- 2016-2020 </h3><br>
-        <h2><u>MEMBERS</u></h2>
-        <h3>Don Idiculla </h3>
-        <h3>Rahul Raj </h3>
-        <h3>Shalom V Mathew </h3>
-        <h3>Sreeraj VR</h3>";
+  echo "<h2><u>TIME PASS PROJECT :- BINGO! (ANDROID GAME)</u></h2>
+        <h2><u>MADE BY</u></h2>
+        <h3>Sreeraj VR (@SVR666)</h3>";
  ?>
